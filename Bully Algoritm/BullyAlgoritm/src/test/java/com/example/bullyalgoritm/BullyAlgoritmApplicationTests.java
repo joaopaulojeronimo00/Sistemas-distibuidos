2b30452037313cffaeade78ca36a9dcb107823e4
João Paulo Jerônimo <joaopaulojeronimo00@gmail.com>
2025-12-15 11:43:50 -0300

    feat: cria bully algorithm

---
+++ Bully Algoritm/BullyAlgoritm/src/test/java/com/example/bullyalgoritm/BullyAlgoritmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bullyalgoritm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullyAlgoritmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
