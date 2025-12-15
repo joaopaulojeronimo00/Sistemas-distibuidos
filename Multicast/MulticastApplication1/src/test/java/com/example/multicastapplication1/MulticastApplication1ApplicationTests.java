33bcd9a85d3fe17827729eface68eccc5fd3face
João Paulo Jerônimo <joaopaulojeronimo00@gmail.com>
2025-12-15 07:32:56 -0300

    add multicast

---
+++ Multicast/MulticastApplication1/src/test/java/com/example/multicastapplication1/MulticastApplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multicastapplication1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulticastApplication1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Multicast/MulticastApplication2/src/test/java/com/example/multicastapplication2/MulticastApplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multicastapplication2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulticastApplication2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
